9711d170157a841e2a095218a7bdc71a7b9a72b6
vlntngrgr <valentin.gregoire91@gmail.com>
2019-02-19 16:01:03 +0100

    Initial commit

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { APP_PORT } = process.env || 5000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.resolve(__dirname, "../../bin/client")));
+
+app.listen(APP_PORT, () =>
+  console.log(`🚀 Server is listening on port ${APP_PORT}.`)
+);
