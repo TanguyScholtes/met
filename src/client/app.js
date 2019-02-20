b732ac13071c6040ac8e3729392db7924e65d124
AnarionBe <marcodb.debona@gmail.com>
2019-02-20 07:04:48 +0100

    startup

--- src/client/app.js
+++ src/client/app.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import Pin from "./components/ColorPin";
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hello World</h1>
+                <Pin />
             </div>
         );
     }
