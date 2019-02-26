ca3b662f9e619089a96fe07a411fc149c631a41a
TanguyScholtes <soultaker720@hotmail.com>
2019-02-26 16:19:49 +0100

    trying to send json with all rooms to front-end

--- src/server/index.js
+++ src/server/index.js
@@ -15,22 +15,23 @@ let rng = function ( min, max ) {
     return Math.floor( Math.random() * ( max - min + 1 ) + min );
 }
 
+let SOCKET_LIST = [];
+let ROOMS_LIST = [];
+const COLORS = [ 'yellow', 'blue', 'red', 'green', 'white', 'black', 'purple', 'pink' ];
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.all("*", (req, res) => {
+    res.json( ROOMS_LIST );
     res.sendFile(__dirname + "../../client/index.html");
 });
 
 http.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
-
-let SOCKET_LIST = [];
-let ROOMS_LIST = [];
-const COLORS = [ 'yellow', 'blue', 'red', 'green', 'white', 'black', 'purple', 'pink' ];
 
 io.sockets.on("connection", socket => {
     socket.id = Math.random();
