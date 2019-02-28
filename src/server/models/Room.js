ff6d0204edd64498eabce8a80209f171026dee4a
Marco De Bona <marcodb.debona@gmail.com>
2019-02-28 09:06:25 +0100

    edit room class

--- src/server/models/Room.js
+++ src/server/models/Room.js
@@ -1,14 +1,14 @@
 class Room {
-
-    constructor ( name, playersNumber, host ) {
+    constructor(name, playersNumber, host) {
         this.id = name;
         this.maxTries = 10;
         this.combination = [];
         this.playersNumber = playersNumber;
         this.players = [];
 
-        this.players[ host.id ] = host;
+        // this.players[host.id] = host;
+        this.host = host;
     }
 }
 
-module.exports = { Room };
+module.exports = {Room};
